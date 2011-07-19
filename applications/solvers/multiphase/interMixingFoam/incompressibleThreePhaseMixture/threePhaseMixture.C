714ff6e85156caff69d09346ab741fc69da38403
Henry <Henry>
2011-07-19 17:05:56 +0100

    Corrected class names in the file descriptions
    Resolves bug-report http://www.openfoam.com/mantisbt/view.php?id=254

--- applications/solvers/multiphase/interMixingFoam/incompressibleThreePhaseMixture/threePhaseMixture.C
+++ applications/solvers/multiphase/interMixingFoam/incompressibleThreePhaseMixture/threePhaseMixture.C
@@ -21,9 +21,6 @@ License
     You should have received a copy of the GNU General Public License
     along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
 
-Class
-    threePhaseMixture
-
 \*---------------------------------------------------------------------------*/
 
 #include "threePhaseMixture.H"
--- applications/utilities/mesh/manipulation/polyDualMesh/meshDualiser.C
+++ applications/utilities/mesh/manipulation/polyDualMesh/meshDualiser.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2004-2010 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2004-2011 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -20,9 +20,6 @@ License
 
     You should have received a copy of the GNU General Public License
     along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
-
-Class
-    meshDualiser
 
 \*---------------------------------------------------------------------------*/
 
