7bbefa1fa0cd09aa854bc3ba009bbd64d040b321
sferraris <sferraris@sgi.com>
2011-09-09 11:30:32 +0100

    BUG: Uptoda fireFoam tutorials and a small change in the collection

--- src/regionModels/pyrolysisModels/noPyrolysis/noPyrolysis.C
+++ src/regionModels/pyrolysisModels/noPyrolysis/noPyrolysis.C
@@ -44,6 +44,16 @@ addToRunTimeSelectionTable(pyrolysisModel, noPyrolysis, dictionary);
 
 // * * * * * * * * * * * * Protected Member Functions  * * * * * * * * * * * //
 
+void noPyrolysis::constructThermoChemistry()
+{
+    solidChemistry_.reset
+    (
+        solidChemistryModel::New(regionMesh()).ptr()
+    );
+
+    solidThermo_.reset(&solidChemistry_->solidThermo());
+}
+
 bool noPyrolysis::read()
 {
     if (pyrolysisModel::read())
@@ -76,10 +86,15 @@ bool noPyrolysis::read(const dictionary& dict)
 
 noPyrolysis::noPyrolysis(const word& modelType, const fvMesh& mesh)
 :
-    pyrolysisModel(modelType, mesh),
-    solidChemistry_(solidChemistryModel::New(regionMesh())),
-    solidThermo_(solidChemistry_->solidThermo())
-{}
+    pyrolysisModel(mesh),
+    solidChemistry_(NULL),
+    solidThermo_(NULL)
+{
+    if (active())
+    {
+        constructThermoChemistry();
+    }
+}
 
 
 noPyrolysis::noPyrolysis
@@ -88,10 +103,15 @@ noPyrolysis::noPyrolysis
     const fvMesh& mesh,
     const dictionary& dict
 ):
-    pyrolysisModel(modelType, mesh, dict),
-    solidChemistry_(solidChemistryModel::New(regionMesh())),
-    solidThermo_(solidChemistry_->solidThermo())
-{}
+    pyrolysisModel(mesh),
+    solidChemistry_(NULL),
+    solidThermo_(NULL)
+{
+    if (active())
+    {
+        constructThermoChemistry();
+    }
+}
 
 // * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
 
@@ -115,31 +135,31 @@ void noPyrolysis::evolveRegion()
 
 const volScalarField& noPyrolysis::rho() const
 {
-    return (solidThermo_.rho());
+    return (solidThermo_->rho());
 }
 
 
 const volScalarField& noPyrolysis::T() const
 {
-    return (solidThermo_.T());
+    return (solidThermo_->T());
 }
 
 
 const tmp<volScalarField> noPyrolysis::Cp() const
 {
-    return (solidThermo_.Cp());
+    return (solidThermo_->Cp());
 }
 
 
 const volScalarField& noPyrolysis::kappa() const
 {
-    return (solidThermo_.kappa());
+    return (solidThermo_->kappa());
 }
 
 
 const volScalarField& noPyrolysis::K() const
 {
-     return (solidThermo_.K());
+     return (solidThermo_->K());
 }
 
 
